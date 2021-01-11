90050e0cd4bd406ef1bc15dc1ff0383d8af678c3
David Coats <david@getsafetycone.com>
2021-01-10 22:31:10 -0800

    Add textarea wrapper

---
+++ source/ts/TextAreaWrapper.ts
@@ -0,0 +1,42 @@
+import { StringHelper } from "r5t-Avignon/Helpers/StringHelper";
+
+
+export class TextAreaWrapper
+{
+    private zTextAreaHtmlElement : HTMLTextAreaElement;
+    public get TextAreaHtmlElement() : HTMLTextAreaElement {
+        return this.zTextAreaHtmlElement;
+    }
+    public set TextAreaHtmlElement(v : HTMLTextAreaElement) {
+        this.zTextAreaHtmlElement = v;
+    }
+
+    public get Value(): string
+    {
+        // I always forget how to get the value of a textarea. Is it value, innerText, innerHtml?
+        let output = this.zTextAreaHtmlElement.value;
+        return output;
+    }
+    public set Value(value: string)
+    {
+        this.zTextAreaHtmlElement.value = value;
+
+        // Trigger the change event, as it should be!
+        let changeEvent = new Event("change");
+
+        this.zTextAreaHtmlElement.dispatchEvent(changeEvent);
+    }
+
+
+    constructor(textAreaHtmlElement: HTMLTextAreaElement)
+    {
+        this.TextAreaHtmlElement = textAreaHtmlElement;
+    }
+
+
+    public HasValue(): boolean
+    {
+        let hasValue = StringHelper.IsNonEmpty(this.zTextAreaHtmlElement.value);
+        return hasValue;
+    }
+}
